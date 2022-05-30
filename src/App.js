b7fb58a26cc28ff73121d66efd834e6ebcf53ea3
VuDej <drvu47@gmail.com>
2022-05-30 20:31:53 +0200

    redux store created, data fetched. rack corse installed for fix conflict between urls

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Homepage from './components/Home';
 import Greeting from './components/Greeting';
+import store from './redux/configureStore';
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/greetings" element={<Greeting />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/greetings" element={<Greeting />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
-/* eslint-disable arrow-body-style */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Homepage = () => {
-  return (
-    <div className="homepage-wrapper">
-      <h1>Testing</h1>
-      <Link to="/greetings" className="button3">Go to Greetings page</Link>
-    </div>
-  );
-};
+class Homepage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Rails-React testing</h1>
+        <Link to="/greetings" className="button3">
+          Go to Greetings page
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default Homepage;
